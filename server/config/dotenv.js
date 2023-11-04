e742492ab1cbadafdd1dde9d929539e22f043f81
Shiva <shiva.khatri0001@gmail.com>
2023-11-04 19:54:49 -0400

    Working on database

--- server/config/dotenv.js
+++ server/config/dotenv.js
@@ -1,3 +1,5 @@
 // This is just to give the address of the .env file which will be used in the database
 import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' })
+dotenv.config({ path: '../.env' }) //after this line it will allow to use process.env.VARIABLE_NAME from .env file we created
+
+
