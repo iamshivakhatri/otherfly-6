7c856e783ebdc3e8ea78e86420d4fd302c4eff20
Shiva <shiva.khatri0001@gmail.com>
2023-11-13 12:05:05 -0500

    Done

--- server/routes/trips_destinations.js
+++ server/routes/trips_destinations.js
@@ -4,7 +4,7 @@ import TripsDestinationsController from '../controllers/trips_destinations.js'
 
 const router = express.Router()
 
-router.get('/', TripsDestinationsController.getTripsDestinations)
+//router.get('/', TripsDestinationsController.getTripsDestinations)
 router.get('/trips/:destination_id', TripsDestinationsController.getAllTrips)
 router.get('/destinations/:trip_id', TripsDestinationsController.getAllDestinations)
 router.post('/', TripsDestinationsController.createTripDestination)
